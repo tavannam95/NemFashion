45c49f7de102463d2416e330bb2caa13a03de938
tavannam95 <namtvph13082@fpt.edu.vn>
2022-08-31 22:26:39 +0700

    all

---
+++ src/main/java/nem/com/NemFashionApplication.java
@@ -0,0 +1,13 @@
+package nem.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NemFashionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NemFashionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nem/com/NemFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package nem.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NemFashionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
