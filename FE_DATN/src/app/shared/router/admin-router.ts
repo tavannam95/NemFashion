d0281d87f3293cd143e96f36b6047e11a0a48194
tavannam95 <namtvph13082@fpt.edu.vn>
2022-09-28 01:49:53 +0700

    Commit pages and shared

---
+++ FE_DATN/src/app/shared/router/admin-router.ts
@@ -0,0 +1,31 @@
+import {Routes} from '@angular/router'
+import {DashboardComponent} from '../../dashboard/dashboard.component';
+import {UserProfileComponent} from '../../user-profile/user-profile.component';
+import {TableListComponent} from '../../table-list/table-list.component';
+import {TypographyComponent} from '../../typography/typography.component';
+import {IconsComponent} from '../../icons/icons.component';
+import {NotificationsComponent} from '../../notifications/notifications.component';
+
+
+export const content_admin: Routes = [
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'user-profile', component: UserProfileComponent},
+    {path: 'table-list', component: TableListComponent},
+    {path: 'typography', component: TypographyComponent},
+    {path: 'icons', component: IconsComponent},
+    {path: 'notifications', component: NotificationsComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'user-profile', component: UserProfileComponent},
+    {path: 'table-list', component: TableListComponent},
+    {path: 'typography', component: TypographyComponent},
+    {path: 'icons', component: IconsComponent},
+    // { path: 'notifications',  component: NotificationsComponent },
+    {
+        path: 'notifications',
+        loadChildren: () => import('../../pages/admin/staff-manager/staff.modules').then(m => m.StaffModules),
+    },
+    {
+        path: 'customer',
+        loadChildren: () => import('../../pages/admin/customer-manager/customer-manager.module').then(m => m.CustomerManagerModule),
+    }
+]
