b32a2dad9f3f15188c580b4f195c0e687ca1aa7e
lethuy2001 <thuylmph1375@fpt.edu.vn>
2022-10-04 15:27:08 +0700

    haha

---
+++ FE/src/app/shared/service/employee/employee-api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {ApiConstant} from '../../constants/ApiConstant';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeApiService {
+
+
+  constructor( private http: HttpClient ) { }
+
+  getAll() : Observable<any> {
+    return this.http.get( ApiConstant.employee ) ;
+  }
+
+  create( employee: any ): Observable<any> {
+    return this.http.post( ApiConstant.employee , employee ) ;
+  }
+
+  update( employee: any ) : Observable<any> {
+    return this.http.put( `${ ApiConstant.employee }/${employee.id}` , employee ) ;
+  }
+}
