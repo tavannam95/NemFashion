ca6c7474784fcd3672eda22f5166f80e9b1a2cf7
phuongtd158 <tdphuong2002@gmail.com>
2022-10-04 15:13:35 +0700

    update

--- FE/src/app/shared/constants/ApiConstant.ts
+++ FE/src/app/shared/constants/ApiConstant.ts
@@ -1,9 +1,5 @@
 export const ApiConstant = {
-<<<<<<< HEAD
-    customer: 'http://localhost:8080/api/v1/customer' ,
-    employee: 'http://localhost:8080/api/v1/employee'
-=======
-    cloudinary: 'http://localhost:8080/api/image',
-    customer: 'http://localhost:8080/api/v1/customer'
->>>>>>> main
-}
+  customer: "http://localhost:8080/api/v1/customer",
+  employee: "http://localhost:8080/api/v1/employee",
+  cloudinary: "http://localhost:8080/api/image",
+};
