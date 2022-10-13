e43f8ebf9f47ecadf43238d830ec07c7f02a5b0b
tavannam95 <namtvph13082@fpt.edu.vn>
2022-10-14 00:08:59 +0700

    NamTV commit Product Image Rest Controller and Product Image API Service

--- FE/src/app/shared/constants/ApiConstant.ts
+++ FE/src/app/shared/constants/ApiConstant.ts
@@ -6,6 +6,7 @@ export const ApiConstant = {
   category: "http://localhost:8080/api/v1/category",
   customer: 'http://localhost:8080/api/v1/customer',
   employee: 'http://localhost:8080/api/v1/employee',
+  productImage: 'http://localhost:8080/api/v1/productImage',
   cloudinary: 'http://localhost:8080/api/image',
   auth: {
         login: 'http://localhost:8080/auth/admin/login',
--- FE/src/app/shared/service/productImage/product-image.service.ts
+++ FE/src/app/shared/service/productImage/product-image.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
+import { ProductImageApiService } from './product-image-api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductImageService {
 
-constructor() { }
+constructor(
+  private apiService: ProductImageApiService
+) { }
+getAllProductImage() {
+  return this.apiService.getAllProductImage();
+}
 
+getProductImage(id: number) {
+  return this.apiService.getProductImage(id);
+}
+createCustomer(data: any) {
+  return this.apiService.createProductImage(data);
+}
 }
