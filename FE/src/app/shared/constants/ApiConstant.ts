2e8c6e04a42eeb243279b96e2e08a2a4f7e54744
tavannam95 <namtvph13082@fpt.edu.vn>
2022-10-14 21:01:26 +0700

    NamTV commit bugfix duplicated

--- FE/src/app/shared/constants/ApiConstant.ts
+++ FE/src/app/shared/constants/ApiConstant.ts
@@ -1,11 +1,8 @@
 export const ApiConstant = {
   customer: "http://localhost:8080/api/v1/customer",
   employee: "http://localhost:8080/api/v1/employee",
-  cloudinary: "http://localhost:8080/api/image",
   product: "http://localhost:8080/api/v1/product",
   category: "http://localhost:8080/api/v1/category",
-  customer: 'http://localhost:8080/api/v1/customer',
-  employee: 'http://localhost:8080/api/v1/employee',
   productImage: 'http://localhost:8080/api/v1/productImage',
   cloudinary: 'http://localhost:8080/api/image',
   auth: {
