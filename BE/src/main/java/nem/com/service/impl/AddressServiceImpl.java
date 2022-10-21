d34e70e9f66c0cb4aac6f1a9d3886d13b071b683
DoThanhHien <74393801+DoThanhHien@users.noreply.github.com>
2022-10-21 22:19:48 +0700

    Hiendtt commit address

---
+++ BE/src/main/java/nem/com/service/impl/AddressServiceImpl.java
@@ -0,0 +1,30 @@
+package nem.com.service.impl;
+
+import nem.com.entity.Address;
+import nem.com.service.AddressService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+    @Override
+    public List<Address> getAll() {
+        return null;
+    }
+
+    @Override
+    public Address getOne(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Address save(Address address) {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
+}
