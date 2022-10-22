48d380fa3ef6e7d2ebb61dd01696d930d775cb33
DoThanhHien <74393801+DoThanhHien@users.noreply.github.com>
2022-10-22 23:04:21 +0700

    Do THI Thanh Hien commit BE Address

--- BE/src/main/java/nem/com/service/impl/AddressServiceImpl.java
+++ BE/src/main/java/nem/com/service/impl/AddressServiceImpl.java
@@ -1,30 +1,39 @@
 package nem.com.service.impl;
 
 import nem.com.entity.Address;
+import nem.com.repository.AddressRepository;
 import nem.com.service.AddressService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class AddressServiceImpl implements AddressService {
+
+    private  final AddressRepository addressRepository;
+
+    public AddressServiceImpl(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
     @Override
     public List<Address> getAll() {
-        return null;
+        return this.addressRepository.findAll();
     }
 
     @Override
     public Address getOne(Integer id) {
-        return null;
+        return this.addressRepository.findById(id).get();
     }
 
     @Override
     public Address save(Address address) {
-        return null;
+        return this.addressRepository.save(address);
     }
 
     @Override
     public void delete(Integer id) {
-
+        this.addressRepository.deleteById(id);
     }
 }
