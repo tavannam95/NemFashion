c69c3036cd70c0274f917abd27e55b972ea8c118
tavannam95 <namtvph13082@fpt.edu.vn>
2022-11-27 21:01:34 +0700

    update

--- FE/src/app/shared/menu/menuItems.ts
+++ FE/src/app/shared/menu/menuItems.ts
@@ -6,12 +6,12 @@ interface RouteInfo {
 }
 
 export const MenuItems: RouteInfo[] = [
-    {path: '/dashboard', title: 'Dashboard', icon: 'fas fa-yin-yang', class: ''},
-    {path: '/statical' , title: 'Thống kê' , icon: 'fas fa-yin-yang' , class: ''},
+    {path: '/dashboard', title: 'Tổng quan', icon: 'fas fa-globe-asia', class: ''},
+    {path: '/statical' , title: 'Thống kê' , icon: 'fas fa-chart-pie' , class: ''},
     {path: '/staff', title: 'Nhân viên', icon: 'fas fa-user-tie', class: ''},
     {path: '/customer', title: 'Khách hàng', icon: 'fas fa-users', class: ''},
     {path: '/order', title: 'Đơn hàng', icon: 'fas fa-clipboard', class: ''},
     {path: '/product', title: 'Sản phẩm', icon: 'fas fa-box', class: ''},
     {path: '/category', title: 'Danh mục', icon: 'fas fa-th-list', class: ''},
-    {path: '/rating' , title: 'Đánh giá' , icon: 'far fa-star' , class: '' }
+    {path: '/rating' , title: 'Đánh giá' , icon: 'fas fa-star' , class: '' }
 ]
